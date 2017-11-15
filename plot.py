03d68b5ac0121a3d6259c1bac24b86743dcbe4f0
Markus Uhr <uhrmar@gmail.com>
2017-11-15 23:09:11 +0100

    Improve plotting script.

--- plot.py
+++ plot.py
@@ -1,32 +1,32 @@
 
+import argparse
 import base64
 import jinja2
 
 from glob import glob
+from pathlib import Path
 
 from sim import parse_dump
 
 
-# rom name
-romname = 'Dragster (1980) (Activision)'
+# parse arguments
+parser = argparse.ArgumentParser(description='Create an interactive SVG.')
+parser.add_argument("runid", help="The identifier of the run.")
+parser.add_argument("--romname", help="The name of the ROM file.", default='Dragster (1980) (Activision)')
+args = parser.parse_args()
 
-# run
-runid = "b205071d"
-# ***DEBUG***
-# for s in states:
-#     print(s)
-# exit(0)
-# ***DEBUG***
+# create output directory
+Path('docs/plots').mkdir(parents=True, exist_ok=True)
 
 # load states
-states = [s for s in parse_dump(romname, runid)]
+states = [s for s in parse_dump(args.romname, args.runid)]
 print(f"number of states: {len(states)}")
 
 # load frames
-frames = [str(base64.b64encode(open(f, "rb").read()), 'ascii') for f in sorted(glob(f"data/{romname}_dbg_{runid}_*.png"))]
+frames = [str(base64.b64encode(open(f, "rb").read()), 'ascii') for f in sorted(glob(f"data/{args.romname}_dbg_{args.runid}_*.png"))]
 print(f"number of frames: {len(frames)}")
 
 # create interactive svg
 env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 template = env.get_template('race.svg.j2')
-template.stream(states=states, frames=frames, maxframe=len(states)).dump(f"race_{runid}.svg")
+template.stream(states=states, frames=frames, maxframe=len(states)).dump(f"docs/plots/race_{args.runid}.svg")

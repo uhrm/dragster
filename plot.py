a13b11ee5d76062dd1f784773ef128d791e65095
Markus Uhr <uhrmar@gmail.com>
2017-11-18 09:56:41 +0100

    Add 'offset' option to skip first <n> frames.

--- plot.py
+++ plot.py
@@ -1,29 +1,31 @@
 
 import argparse
-import base64
 import jinja2
 
+from base64 import b64encode
 from glob import glob
+from itertools import islice
 from pathlib import Path
 
 from sim import parse_dump
 
 
 # parse arguments
-parser = argparse.ArgumentParser(description='Create an interactive SVG.')
+parser = argparse.ArgumentParser(description="Create an interactive SVG.")
 parser.add_argument("runid", help="The identifier of the run.")
-parser.add_argument("--romname", help="The name of the ROM file.", default='Dragster (1980) (Activision)')
+parser.add_argument("--romname", default="Dragster (1980) (Activision)", help="The name of the ROM file.")
+parser.add_argument("--offset", type=int, default=0, help="Skip the first <n> frames.")
 args = parser.parse_args()
 
 # create output directory
 Path('pages/plots').mkdir(parents=True, exist_ok=True)
 
 # load states
-states = [s for s in parse_dump(args.romname, args.runid)]
+states = [s for s in islice(parse_dump(args.romname, args.runid), args.offset, None)]
 print(f"number of states: {len(states)}")
 
 # load frames
-frames = [str(base64.b64encode(open(f, "rb").read()), 'ascii') for f in sorted(glob(f"data/{args.romname}_dbg_{args.runid}_*.png"))]
+frames = [str(b64encode(open(f, "rb").read()), 'ascii') for f in islice(sorted(glob(f"data/{args.romname}_dbg_{args.runid}_*.png")), args.offset, None)]
 print(f"number of frames: {len(frames)}")
 
 # create interactive svg

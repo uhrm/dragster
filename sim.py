4b73ebeeeaf0306d14f7567d7a7ad0dd05794137
Markus Uhr <uhrmar@gmail.com>
2017-11-18 13:11:19 +0100

    Multiple simulation improvements.
     - Implement 'frame offset' feature (even frames only).
     - Refactor input generators.
     - Fix 'time up' and 'busted' end conditions.

--- sim.py
+++ sim.py
@@ -45,8 +45,12 @@ def vref(y, r):
     return ((1 << y) >> 1)*r + ((1 << y) >> 2)*(r >= 20)
 
 
-def sim(fth, fcl, offset=0, verbose=0):
+def sim(ingen, offset=0, verbose=0):
     """Simulate the game state."""
+
+    # TODO: also support odd start frames
+    if (offset & 0x01) != 0:
+        ValueError(f"Odd numbered start frames are not supported: offset = {offset}.")
 
     # time
     t = 0
@@ -59,9 +63,7 @@ def sim(fth, fcl, offset=0, verbose=0):
     y = 0  # gear
     r = 0  # motor rpm
 
-    # input generators
-    thgen = fth()
-    clgen = fcl()
+    busted = False
 
     # inputs
     th = thprev = 0
@@ -69,19 +71,17 @@ def sim(fth, fcl, offset=0, verbose=0):
 
     # start game (hard-coded)
     yield FrameState(frame=t, status=0x0100, countdown=tm, time=tr, x=x, v=v, y=1, r=r, th=0, cl=0, rs=0)
-    t += 1
-    # TODO: advance to <offset> frames
-    yield FrameState(frame=t, status=0x0100, countdown=tm, time=tr, x=x, v=v, y=1, r=r, th=0, cl=0, rs=0)
-    t += 1
-    yield FrameState(frame=t, status=0x0100, countdown=tm, time=tr, x=x, v=v, y=1, r=r, th=0, cl=0, rs=0)
+    for s in range(offset):
+        t += 1
+        th, cl = next(ingen)
+        yield FrameState(frame=t, status=0x0100, countdown=tm, time=tr, x=x, v=v, y=1, r=r, th=th, cl=cl, rs=0)
     t += 1
     tm = 159
-    yield FrameState(frame=t, status=0, countdown=tm, time=tr, x=x, v=v, y=y, r=r, th=0, cl=0, rs=1)
+    th, cl = next(ingen)
+    yield FrameState(frame=t, status=0, countdown=tm, time=tr, x=x, v=v, y=y, r=r, th=th, cl=cl, rs=1)
     t += 1
     tm = max(tm-1, 0)
-    yield FrameState(frame=t, status=0, countdown=tm, time=tr, x=x, v=v, y=y, r=r, th=0, cl=0, rs=1)
-    t += 1
-    tm = max(tm-1, 0)
+    th, cl = next(ingen)
 
     # main loop
     while True:
@@ -90,10 +90,6 @@ def sim(fth, fcl, offset=0, verbose=0):
             if verbose > 0:
                 print(f"{t:6d}  {t//16:5d}  EARLY")
             break  # early
-        if t > 1496:
-            if verbose > 0:
-                print(f"{t:6d}  {99.99:5.2f}  TIME UP")
-            break  # time up
         # print state
         if verbose > 0:
             printx(t, x, v, y, r, thprev, clprev)
@@ -102,15 +98,26 @@ def sim(fth, fcl, offset=0, verbose=0):
             frame=t,
             status=0,
             countdown=tm,
-            time=tr,  # max((t-161),0)//2*334,
+            time=tr,
             x=x, v=v, y=y, r=r, th=th, cl=cl, rs=0)
         # update timer
         t += 1
         tm = max(tm-1, 0)
         tr = tr if tm > 0 else 334*(t & 0x01) if tr == 1111000 else tr+334 if t & 0x01 == 1 else tr
+        # check for max time
+        if tm == 0 and tr > 999999:
+            if verbose > 0:
+                print(f"{t:6d}  {99.99:5.2f}  TIME UP")
+            # yield 'time up' state and quit
+            yield FrameState(
+                frame=t,
+                status=0x0100,  # time up
+                countdown=tm,
+                time=999900,
+                x=x, v=v, y=y, r=0, th=th, cl=cl, rs=0)
+            break  # time up
         # current inputs
-        th = next(thgen)
-        cl = next(clgen)
+        th, cl = next(ingen)
         # do state updates in odd frames only
         if t & 0x01 == 1:
             # update distance
@@ -124,7 +131,7 @@ def sim(fth, fcl, offset=0, verbose=0):
                     frame=t,
                     status=0x0100,  # finished
                     countdown=tm,
-                    time=tr,  # max((t-161),0)//2*334,
+                    time=tr,
                     x=x, v=v, y=y, r=0, th=th, cl=cl, rs=0)
                 break
             # update motor RPM
@@ -139,19 +146,8 @@ def sim(fth, fcl, offset=0, verbose=0):
                 r = max(r, 0)
             # check for 'busted'
             if r >= 32:  # r >= 0x20
-                if verbose > 0:
-                    if t < 0:
-                        print(f"{t:6d}  {t//16:5d}  BUSTED")
-                    else:
-                        print(f"{t:6d}  {t*668/10000:5.2f}  BUSTED")
-                # yield 'busted' state and quit
-                yield FrameState(
-                    frame=t,
-                    status=1,  # busted
-                    countdown=tm,
-                    time=max((t-161),0)//2*334,
-                    x=x, v=v, y=y, r=r, th=th, cl=cl, rs=0)
-                break
+                r = 0
+                busted = True
             # update speed
             if y > 0 and clprev == 0:
                 vr = vref(y, r)
@@ -161,6 +157,21 @@ def sim(fth, fcl, offset=0, verbose=0):
                     if vr-v >= 16:
                         r -= 1
                     v += 2
+            # check for 'busted'
+            if busted:
+                if verbose > 0:
+                    if t < 0:
+                        print(f"{t:6d}  {t//16:5d}  BUSTED")
+                    else:
+                        print(f"{t:6d}  {t*668/10000:5.2f}  BUSTED")
+                # yield 'busted' state and quit
+                yield FrameState(
+                    frame=t,
+                    status=0x0001,  # busted
+                    countdown=tm,
+                    time=tr,
+                    x=x, v=v, y=y, r=0, th=th, cl=cl, rs=0)
+                break
             # update gear
             if cl == 0 and clprev == 1:
                 y = min(y+1, 4)
@@ -173,7 +184,7 @@ def sim(fth, fcl, offset=0, verbose=0):
         # ***ENDEBUG***
 
 
-def write_script(script, fth, fcl, offset=0):
+def write_script(script, ingen, offset=0):
     """Write a Stella debug script for the given user inputs."""
 
     # previous inputs
@@ -181,7 +192,7 @@ def write_script(script, fth, fcl, offset=0):
     clprev = 0
     rsprev = 0
 
-    for s in sim(fth, fcl, offset):
+    for s in sim(ingen, offset):
         # inputs
         if s.th != thprev:
             script.write("joy0fire\n")
@@ -239,47 +250,58 @@ def parse_dump(romname, runid):
 
 if __name__ == "__main__":
 
-    # throttle generator
-    def fth():
-        t = 0
-        while t < 144: yield 0; t += 1
-        while True:    yield 1; t += 1
-#        while t <  30: yield 1; t += 1
+    import argparse
+    from collections import deque
+    from pathlib import Path
 
-    # clutch generator
-    def fcl():
-        t = 0
-        while t < 142: yield 0; t += 1
-        while t < 160: yield 1; t += 1
-        while t < 174: yield 0; t += 1
-        while t < 176: yield 1; t += 1
-        while t < 212: yield 0; t += 1
-        while t < 214: yield 1; t += 1
-        while t < 268: yield 0; t += 1
-        while t < 270: yield 1; t += 1
-        while True:    yield 0; t += 1
+    # parse arguments
+    parser = argparse.ArgumentParser(description="Create an interactive SVG.")
+    parser.add_argument("runid", help="The identifier of the run.")
+    parser.add_argument("--romname", default="Dragster (1980) (Activision)", help="The name of the ROM file.")
+    parser.add_argument("--offset", type=int, default=0, help="Start frame offset (0-15).")
+    parser.add_argument("--check-dump", action="store_true", help="Check simulation with Stella dump.")
+    args = parser.parse_args()
+
+    # demo input generator
+    def demogen(offset):
+        thtoggle = deque([148])
+        cltoggle = deque([146, 164, 178, 180, 216, 218, 272, 274])
+        th = 0
+        cl = 0
+        t = 1  # inputs start at frame 1
+        while True:
+            if len(thtoggle) > 0 and thtoggle[0]+offset == t:
+                thtoggle.popleft()
+                th = 1 - th
+            if len(cltoggle) > 0 and cltoggle[0]+offset == t:
+                cltoggle.popleft()
+                cl = 1 - cl
+            yield th, cl
+            t += 1
 
     # write Stella debug script
-    with open('dragster.script', 'w') as script:
-        write_script(script, fth, fcl)
+    Path('scripts').mkdir(parents=True, exist_ok=True)  # create 'scripts' directory
+    with open(f"scripts/{args.runid}.script", "w") as script:
+        write_script(script, demogen(args.offset), args.offset)
 
-    # compare dump with simulation
-    ndiffs = 0
-    for ss, sd in zip(sim(fth, fcl), parse_dump('Dragster (1980) (Activision)', 'demo')):
-        if (ss.status != sd.status) or (ss.time != sd.time) or (ss.countdown != sd.countdown) or \
-           (ss.x != sd.x) or (ss.v != sd.v) or (ss.y != sd.y) or (ss.r != sd.r) or (ss.th != sd.th):
-            ndiffs += 1
-            print(f"frame     {ss.frame:8d}  --  frame     {sd.frame:8d}")
-            print(f"status    {ss.status:8d}  --  status    {sd.status:8d}")
-            print(f"countdown {ss.countdown:8d}  --  countdown {sd.countdown:8d}")
-            print(f"time      {ss.time:8d}  --  time      {sd.time:8d}")
-            print(f"x         {ss.x:8d}  --  x         {sd.x:8d}")
-            print(f"v         {ss.v:8d}  --  v         {sd.v:8d}")
-            print(f"y         {ss.y:8d}  --  y         {sd.y:8d}")
-            print(f"r         {ss.r:8d}  --  r         {sd.r:8d}")
-            print(f"th        {ss.th:8d}  --  th        {sd.th:8d}")
-            print(f"cl        {ss.cl:8d}  --  cl        {sd.cl:8d}")
-            print(f"rs        {ss.rs:8d}  --  rs        {sd.rs:8d}")
-            print()
-    if ndiffs == 0:
-        print('Frame states are equal.')
+    if args.check_dump:
+        # compare dump with simulation
+        ndiffs = 0
+        for ss, sd in zip(sim(demogen(args.offset), args.offset), parse_dump(args.romname, args.runid)):
+            if (ss.status != sd.status) or (ss.time != sd.time) or (ss.countdown != sd.countdown) or \
+               (ss.x != sd.x) or (ss.v != sd.v) or (ss.y != sd.y) or (ss.r != sd.r) or (ss.th != sd.th):
+                ndiffs += 1
+                print(f"frame     {ss.frame:8d}  --  frame     {sd.frame:8d}")
+                print(f"status    {ss.status:8d}  --  status    {sd.status:8d}")
+                print(f"countdown {ss.countdown:8d}  --  countdown {sd.countdown:8d}")
+                print(f"time      {ss.time:8d}  --  time      {sd.time:8d}")
+                print(f"x         {ss.x:8d}  --  x         {sd.x:8d}")
+                print(f"v         {ss.v:8d}  --  v         {sd.v:8d}")
+                print(f"y         {ss.y:8d}  --  y         {sd.y:8d}")
+                print(f"r         {ss.r:8d}  --  r         {sd.r:8d}")
+                print(f"th        {ss.th:8d}  --  th        {sd.th:8d}")
+                print(f"cl        {ss.cl:8d}  --  cl        {sd.cl:8d}")
+                print(f"rs        {ss.rs:8d}  --  rs        {sd.rs:8d}")
+                print()
+        if ndiffs == 0:
+            print('Frame states are equal.')

67c71c1905857abdef918af98226031d78ddddab
Markus Uhr <uhrmar@gmail.com>
2018-01-28 13:45:43 +0100

    Various improvements in optimization module.

--- optim.py
+++ optim.py
@@ -1,6 +1,15 @@
 
+import argparse
 import gurobipy as grb
 from gurobipy import GRB
+
+
+# parse arguments
+parser = argparse.ArgumentParser(description="Compute optimal Dragster inputs.")
+# parser.add_argument("--romname", default="Dragster (1980) (Activision)", help="The name of the ROM file.")
+parser.add_argument("--offset", type=int, default=0, help="Global frame counter offset for starting game.")
+parser.add_argument("--nsteps", type=int, default=177, help="Number of time steps to optimize.")
+args = parser.parse_args()
 
 
 # basic model
@@ -12,8 +21,9 @@ from gurobipy import GRB
 
 # parameters
 
-offset = 0  # offset (in time steps) of game start relative to global frame counter [0-7]
-n = 60  # number of time steps
+offset = args.offset  # offset (in time steps) of game start relative to global frame counter [0-7]
+# n = 66  # number of time steps
+n = args.nsteps  # number of time steps (default 177 corresponds to a 5.57 finish time)
 
 def frame(j, offset):
     """Convert time step j to in-game frame number."""
@@ -67,7 +77,7 @@ vr = [[0]*n,
       [0] + [mod.addVar(vtype=GRB.INTEGER, name=f"vr(4,{j})") for j in range(1,n)]]
 
 # dragster speed
-v = [0]*10 + [mod.addVar(vtype=GRB.INTEGER, obj=1.0, name=f"v({j})") for j in range(10,n)]
+v = [0]*10 + [mod.addVar(vtype=GRB.INTEGER, obj=1.0 if j<n-1 else 0.0, name=f"v({j})") for j in range(10,n)]
 # dragster speed increment
 vd = [[None] + [mod.addVar(vtype=GRB.BINARY, name=f"vd(0,{j})") for j in range(1,n)],
       [None] + [mod.addVar(vtype=GRB.BINARY, name=f"vd(1,{j})") for j in range(1,n)]]
@@ -87,11 +97,21 @@ for j in range(1, n):
     mod.addConstr(yc[j], GRB.LESS_EQUAL, u[1][j-1], f"Sc1({j}")
     mod.addConstr(yc[j], GRB.LESS_EQUAL, 1-u[1][j], f"Sc2({j}")
     mod.addConstr(yc[j], GRB.GREATER_EQUAL, u[1][j-1]-u[1][j], f"Sc3({j}")
-    for i in range(5):
+    # y[0]
+    mod.addConstr(y[0][j], GRB.GREATER_EQUAL, y[0][j-1]-yc[j], f"S1(0,{j})")
+    mod.addConstr(y[0][j], GRB.LESS_EQUAL, y[0][j-1]+yc[j], f"S3(0,{j})")
+    mod.addConstr(y[0][j], GRB.LESS_EQUAL, 1-yc[j], f"S4(0,{j})")
+    # y[1] - y[3]
+    for i in range(1, 4):
         mod.addConstr(y[i][j], GRB.GREATER_EQUAL, y[i][j-1]-yc[j], f"S1({i},{j})")
         mod.addConstr(y[i][j], GRB.GREATER_EQUAL, y[i-1][j-1]-1+yc[j], f"S2({i},{j})")
         mod.addConstr(y[i][j], GRB.LESS_EQUAL, y[i][j-1]+yc[j], f"S3({i},{j})")
         mod.addConstr(y[i][j], GRB.LESS_EQUAL, y[i-1][j-1]+1-yc[j], f"S4({i},{j})")
+    # y[4]
+    mod.addConstr(y[4][j], GRB.GREATER_EQUAL, y[4][j-1]-yc[j], f"S1(4,{j})")
+    mod.addConstr(y[4][j], GRB.GREATER_EQUAL, y[3][j-1]+y[4][j-1]-1+yc[j], f"S2(4,{j})")
+    mod.addConstr(y[4][j], GRB.LESS_EQUAL, y[4][j-1]+yc[j], f"S3(4,{j})")
+    mod.addConstr(y[4][j], GRB.LESS_EQUAL, y[3][j-1]+y[4][j-1]+1-yc[j], f"S4(4,{j})")
 
 # gear/motor speed coupling
 #
@@ -121,20 +141,20 @@ for j in range(1, n):
 #
 rpm_skip = [0x00, 0x00, 0x02, 0x06, 0x0E]
 for j in range(n):
-    mod.addConstr(rd[0][j], GRB.LESS_EQUAL, -yr[0][j] + 2*u[0][j], f"Rd1(0,{j}")
-    mod.addConstr(rd[0][j], GRB.GREATER_EQUAL, yr[0][j] + 2*u[0][j] - 2, f"Rd2(0,{j}")
-    mod.addConstr(rd[0][j], GRB.LESS_EQUAL, yr[0][j], f"Rd3(0,{j}")
-    mod.addConstr(rd[0][j], GRB.GREATER_EQUAL, -yr[0][j], f"Rd3(0,{j}")
+    mod.addConstr(rd[0][j], GRB.LESS_EQUAL, -yr[0][j] + 2*u[0][j], f"Rd1(0,{j})")
+    mod.addConstr(rd[0][j], GRB.GREATER_EQUAL, yr[0][j] + 2*u[0][j] - 2, f"Rd2(0,{j})")
+    mod.addConstr(rd[0][j], GRB.LESS_EQUAL, yr[0][j], f"Rd3(0,{j})")
+    mod.addConstr(rd[0][j], GRB.GREATER_EQUAL, -yr[0][j], f"Rd3(0,{j})")
     Rdrhs = (2*u[0][j], 2*u[0][j]-2, 0, 0)
     for i in range(1, 5):
         if frame(j, offset) & rpm_skip[i] == 0:
             Rdrhs = (-yr[i][j]+Rdrhs[0], yr[i][j]+Rdrhs[1], yr[i][j]+Rdrhs[2], -yr[i][j]+Rdrhs[3])
-    mod.addConstr(rd[1][j], GRB.LESS_EQUAL, Rdrhs[0], f"Rd1(1,{j}")
-    mod.addConstr(rd[1][j], GRB.GREATER_EQUAL, Rdrhs[1], f"Rd2(1,{j}")
-    mod.addConstr(rd[1][j], GRB.LESS_EQUAL, Rdrhs[2], f"Rd3(1,{j}")
-    mod.addConstr(rd[1][j], GRB.GREATER_EQUAL, Rdrhs[3], f"Rd3(1,{j}")
+    mod.addConstr(rd[1][j], GRB.LESS_EQUAL, Rdrhs[0], f"Rd1(1,{j})")
+    mod.addConstr(rd[1][j], GRB.GREATER_EQUAL, Rdrhs[1], f"Rd2(1,{j})")
+    mod.addConstr(rd[1][j], GRB.LESS_EQUAL, Rdrhs[2], f"Rd3(1,{j})")
+    mod.addConstr(rd[1][j], GRB.GREATER_EQUAL, Rdrhs[3], f"Rd3(1,{j})")
     # mod.addConstr(r[j], GRB.EQUAL, (r[j-1] if j > 0 else 0) + 3*rd[0][j] + rd[1][j], f"R({j})")
-    mod.addConstr(r[j], GRB.EQUAL, (r[j-1] if j > 0 else 0) + 3*rd[0][j] + rd[1][j] - rv[j], f"R({j})")
+    mod.addConstr(r[j], GRB.EQUAL, (r[j-1] if j > 0 else 0) + 3*rd[0][j] + rd[1][j] - rv[j-1], f"R({j})")
 
 # turbocharger flag
 #
@@ -146,11 +166,11 @@ for j in range(1, n):
 
 # ref speed
 #
-#   yr[0]=1:  vr[0] = 0
-#   yr[1]=1:  vr[1] = r
-#   yr[2]=1:  vr[2] = 2*r + c
-#   yr[3]=1:  vr[3] = 4*r + 2*c
-#   yr[4]=1:  vr[4] = 8*r + 4*c
+#   vr[0][j] = 0                  if yr[0][j] = 1
+#   vr[1][j] = r[j]               if yr[1][j] = 1
+#   vr[2][j] = 2*r[j] + c[j]      if yr[2][j] = 1
+#   vr[3][j] = 4*r[j] + 2*c[j]    if yr[3][j] = 1
+#   vr[4][j] = 8*r[j] + 4*c[j]    if yr[4][j] = 1
 #
 for j in range(1, n):
     # y = 1
@@ -195,37 +215,37 @@ for j in range(1, n):
     mod.addConstr(v[j-1] + 252*(yr[0][j]+rv[j]), GRB.GREATER_EQUAL, vr[1][j]+vr[2][j]+vr[3][j]+vr[4][j]-15, "")
     mod.addConstr(v[j-1] - 252*(1-rv[j]), GRB.LESS_EQUAL, vr[1][j]+vr[2][j]+vr[3][j]+vr[4][j]-16, "")
 
+# symmetry breaking
+#
+#   u[th][j] = 1    if sum(yr[i][j]*(j&mask[i]=1),i=1,..,4)==1
+#
+for j in range(1, n):
+    Srhs = 0
+    for i in range(1, 5):
+        if (frame(j, offset) & rpm_skip[i]) != 0:
+            Srhs = Srhs + yr[i][j]
+    mod.addConstr(u[0][j], GRB.GREATER_EQUAL, Srhs, f"S{j}")
+
+
 status = mod.optimize()
 
-if mod.status == GRB.OPTIMAL:
-    print("frame:   ", str.join(" ", (f"{frame(j, offset):4d}" for j in range(n))))
-    print("j:       ", str.join(" ", (f"{j:4d}" for j in range(n))))
-    print("u(th):   ", str.join(" ", (f"{int(uj.x):4d}" for uj in u[0])))
-    print("u(cl):   ", str.join(" ", (f"{int(uj.x):4d}" for uj in u[1])))
-    print("y(0):    ", str.join(" ", (f"{int(yj.x) if j >= 10 else yj:4d}" for j, yj in enumerate(y[0]))))
-    print("y(1):    ", str.join(" ", (f"{int(yj.x) if j >= 10 else yj:4d}" for j, yj in enumerate(y[1]))))
-    print("y(2):    ", str.join(" ", (f"{int(yj.x) if j >= 10 else yj:4d}" for j, yj in enumerate(y[2]))))
-    print("y(3):    ", str.join(" ", (f"{int(yj.x) if j >= 10 else yj:4d}" for j, yj in enumerate(y[3]))))
-    print("y(4):    ", str.join(" ", (f"{int(yj.x) if j >= 10 else yj:4d}" for j, yj in enumerate(y[4]))))
-    print("yc:      ", str.join(" ", (f"{int(ycj.x) if j >= 1 else 0:4d}" for j, ycj in enumerate(yc))))
-    print("yr(0):   ", str.join(" ", (f"{int(yrj.x) if j >= 1 else yrj:4d}" for j, yrj in enumerate(yr[0]))))
-    print("yr(1):   ", str.join(" ", (f"{int(yrj.x) if j >= 1 else yrj:4d}" for j, yrj in enumerate(yr[1]))))
-    print("yr(2):   ", str.join(" ", (f"{int(yrj.x) if j >= 1 else yrj:4d}" for j, yrj in enumerate(yr[2]))))
-    print("yr(3):   ", str.join(" ", (f"{int(yrj.x) if j >= 1 else yrj:4d}" for j, yrj in enumerate(yr[3]))))
-    print("yr(4):   ", str.join(" ", (f"{int(yrj.x) if j >= 1 else yrj:4d}" for j, yrj in enumerate(yr[4]))))
-    print("rd(N):   ", str.join(" ", (f"{int(rdj.x):4d}" for rdj in rd[0])))
-    print("rd(G):   ", str.join(" ", (f"{int(rdj.x):4d}" for rdj in rd[1])))
-    print("r:       ", str.join(" ", (f"{int(rj.x):4d}" for rj in r)))
-    print("c:       ", str.join(" ", (f"{int(cj.x) if j >= 1 else cj:4d}" for j, cj in enumerate(c))))
-    print("vr(0):   ", str.join(" ", (f"{int(vrj):4d}" for vrj in [0]*n)))
-    print("vr(1):   ", str.join(" ", (f"{int(vrj.x) if j >= 1 else vrj:4d}" for j, vrj in enumerate(vr[1]))))
-    print("vr(2):   ", str.join(" ", (f"{int(vrj.x) if j >= 1 else vrj:4d}" for j, vrj in enumerate(vr[2]))))
-    print("vr(3):   ", str.join(" ", (f"{int(vrj.x) if j >= 1 else vrj:4d}" for j, vrj in enumerate(vr[3]))))
-    print("vr(4):   ", str.join(" ", (f"{int(vrj.x) if j >= 1 else vrj:4d}" for j, vrj in enumerate(vr[4]))))
-    print("vd(inc): ", str.join(" ", (f"{int(vdj.x) if j >= 1 else 0:4d}" for j, vdj in enumerate(vd[0]))))
-    print("vd(dec): ", str.join(" ", (f"{int(vdj.x) if j >= 1 else 0:4d}" for j, vdj in enumerate(vd[1]))))
-    print("v:       ", str.join(" ", (f"{int(vj.x) if j >= 10 else vj:4d}" for j, vj in enumerate(v))))
-    print("rv:      ", str.join(" ", (f"{int(rvj.x) if j >= 1 else rvj:4d}" for j, rvj in enumerate(rv))))
+if (mod.status == GRB.OPTIMAL) or (mod.status == GRB.INTERRUPTED) or (mod.status == GRB.SUBOPTIMAL):
+    print("j    frame  th  cl  y  yc  yr  rd  r   c  vr   vd  v    rv")
+    for j in range(n):
+        print(f"{j:<3d}  "
+              f"{frame(j, offset):<5d}  "
+              f"{int(u[0][j].x):<2d}  "
+              f"{int(u[1][j].x):<2d}  "
+              f"{sum(i*(int(y[i][j].x) if j >= 10 else y[i][j]) for i in range(5)):d}  "
+              f"{int(yc[j].x) if j >= 1 else 0:<2d}  "
+              f"{sum(i*(int(yr[i][j].x) if j >= 1 else yr[i][j]) for i in range(5)):<2d}  "
+              f"{3*int(rd[0][j].x)+int(rd[1][j].x):2d}  "
+              f"{int(r[j].x):<2d}  "
+              f"{int(c[j].x) if j >= 1 else c[j]:d}  "
+              f"{sum(int(vr[i][j].x) if j >= 1 else vr[i][j] for i in range(1, 5)):<3d}  "
+              f"{2*(int(vd[0][j].x) if j >= 1 else 0)-(int(vd[1][j].x) if j >= 1 else 0):2d}  "
+              f"{int(v[j].x) if j >= 10 else v[j]:<3d}  "
+              f"{int(rv[j].x) if j >= 1 else rv[j]:d}")
 elif (mod.status == GRB.INF_OR_UNBD) or (mod.status == GRB.INFEASIBLE):
     # mod.computeIIS()
     # mod.write("%s.ilp" % "tmp")
@@ -251,6 +271,6 @@ def optgen(offset):
 
 # write Stella debug script
 Path('scripts').mkdir(parents=True, exist_ok=True)  # create 'scripts' directory
-with open(f"scripts/opt_ofs{2*offset:x}.script", "w") as script:
-    write_script(script, optgen(0), 0, frame(n, offset)-1)
+with open(f"scripts/opt{n:03d}_ofs{2*offset:x}.script", "w") as script:
+    write_script(script, optgen(offset), offset, frame(n, offset)-1)
 
